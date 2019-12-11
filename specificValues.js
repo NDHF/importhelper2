048f6ca424f6778056d1898e41c22d63c5485ef0
Nicholas Bernhard <nicholas@NDHFilms.com>
2019-12-10 21:28:46 -0700

    Fleshing out specific values object

--- specificValues.js
+++ specificValues.js
@@ -1,3 +1,9 @@
 const specificValues = {
-
+    companyName: "",
+    momFolderValidationString: "",
+    adminFolderValidationString: "",
+    arrayOfProductsWhichMightNeedSpecialPacking: [],
+    specialOrderCode: "",
+    specialLaborCode1: "",
+    specialLaborCode2: ""
 };
